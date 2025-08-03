531e8f9b3ec0790f220be1046fbb9c05039f7f5e
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-08-03 12:14:14 +0900

    Update variables.tf

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,1 +1,22 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "allowed_ip" {
+  description = "Allowed IP for VPN access"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+}
 
