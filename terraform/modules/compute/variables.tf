0fa151a163612070f3815e789d0e7003b75c9eef
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-08-03 12:11:55 +0900

    Update variables.tf

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,1 +1,29 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
 
+variable "public_subnet_id" {
+  description = "Public subnet ID"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "Private subnet ID"
+  type        = string
+}
+
+variable "vpn_security_group_id" {
+  description = "VPN security group ID"
+  type        = string
+}
+
+variable "web_security_group_id" {
+  description = "Web security group ID"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+}
