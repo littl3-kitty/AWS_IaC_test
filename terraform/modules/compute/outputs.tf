27a650895a6d1c505a7cc8b644e80488b7ddc501
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-08-03 12:12:06 +0900

    Update outputs.tf

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,1 +1,15 @@
+output "vpn_public_ip" {
+  value = aws_eip.vpn.public_ip
+}
 
+output "vpn_instance_id" {
+  value = aws_instance.vpn.id
+}
+
+output "web_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "web_private_ip" {
+  value = aws_instance.web.private_ip
+}
