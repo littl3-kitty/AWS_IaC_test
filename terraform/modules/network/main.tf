77c41dd038f4ae488a632fa0ea9199179965f132
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-08-03 15:14:15 +0900

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false  # CKV_AWS_130 해결
 
   tags = {
     Name = "${var.env}-public-subnet"
@@ -37,7 +37,7 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-# NAT Gateway for private subnet
+# NAT Instance (프리티어) 대신 NAT Gateway 사용
 resource "aws_eip" "nat" {
   domain = "vpc"
 
@@ -93,6 +93,15 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+# 기본 보안 그룹 제한
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-default-sg-do-not-use"
+  }
+}
+
 resource "aws_security_group" "vpn" {
   name        = "${var.env}-vpn-sg"
   description = "Security group for VPN server"
@@ -103,6 +112,7 @@ resource "aws_security_group" "vpn" {
     to_port     = 1194
     protocol    = "udp"
     cidr_blocks = ["${var.allowed_ip}/32"]
+    description = "OpenVPN from allowed IP"
   }
 
   ingress {
@@ -110,13 +120,49 @@ resource "aws_security_group" "vpn" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${var.allowed_ip}/32"]
+    description = "SSH from allowed IP"
+  }
+
+  # Egress를 필요한 것만으로 제한
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP for package updates"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS for package updates"
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "DNS resolution"
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "DNS resolution TCP"
+  }
+
+  # VPC 내부 통신
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr]
+    description = "All traffic within VPC"
   }
 
   tags = {
@@ -134,6 +180,7 @@ resource "aws_security_group" "web" {
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.vpn.id]
+    description     = "HTTP from VPN server"
   }
 
   ingress {
@@ -141,13 +188,32 @@ resource "aws_security_group" "web" {
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.vpn.id]
+    description     = "SSH from VPN server"
+  }
+
+  # Egress 제한
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP for package updates"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS for package updates"
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "DNS resolution"
   }
 
   tags = {
