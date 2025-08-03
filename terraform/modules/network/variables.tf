5186115c6c5bab50d1482c0941bff5c23af89c49
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-08-03 12:11:10 +0900

    Update variables.tf

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,1 +1,27 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
 
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "allowed_ip" {
+  description = "Allowed IP for VPN access"
+  type        = string
+}
